6545a7496dfc7713bae374fc0175c913756dd52f
Sandro Ubuntu <sandro.suknidze.1@btu.edu.ge>
2024-08-25 22:46:04 +0400

    Test Change

--- src/main/java/com/sandro/ecommercebackendspring/user/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/sandro/ecommercebackendspring/user/repository/PasswordResetTokenRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
     void deleteByUserId(Long userId);
-
 }
