a50954cc5e9b271fef9e32c726cde7ee6d3438eb
Sandro Ubuntu <sandro.suknidze.1@btu.edu.ge>
2024-09-14 16:09:07 +0400

    / token validity to 365 days

--- src/main/java/com/sandro/ecommercebackendspring/jwt/JWTService.java
+++ src/main/java/com/sandro/ecommercebackendspring/jwt/JWTService.java
@@ -41,7 +41,7 @@ public class JWTService {
                 .add(claims)
                 .subject(email)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 10))
+                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 24 * 365))
                 .and()
                 .signWith(getKey())
                 .compact();
