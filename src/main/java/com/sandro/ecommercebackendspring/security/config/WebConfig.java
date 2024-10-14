1cfbce9e1dc7814b599a4138c7e9c8768c407208
Sandro Ubuntu <sandro.suknidze.1@btu.edu.ge>
2024-10-14 22:48:56 +0400

    + global origin

---
+++ src/main/java/com/sandro/ecommercebackendspring/security/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.sandro.ecommercebackendspring.security.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://ecommerce-frontend-kappa-ten.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
